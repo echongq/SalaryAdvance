cdf3bc0513b868bc680a391c9201223461efe2bd
Ernesto Chong <exechong@bancobci.pe>
2024-12-23 03:27:26 -0500

    clean

--- SalaryAdvance/Domain/DTO/CreateEmployeeRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SalaryAdvance.Domain.DTO
-{
-    public class CreateEmployeeRequest
-    {
-        public string Name { get; set; } = string.Empty;
-        public decimal MonthlySalary { get; set; }
-    }
-}
